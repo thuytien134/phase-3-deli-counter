5523e8b4a4b35a8978c0d3a63c0c04feea49df6a
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-07-11 15:31:34 -0700

    completed

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+require "pry"
+
+def line(arr)
+  if arr.size == 0
+    puts "The line is currently empty."
+  else
+    my_arr = arr.map.with_index do |name, index|
+      "#{index + 1}. #{name}"
+    end
+    puts "The line is currently: #{my_arr.join(" ")}"
+  end
+end
+
+def take_a_number(arr, name)
+  arr << name
+  puts "Welcome, #{name}. You are number #{arr.length} in line."
+end
+
+def now_serving(arr)
+  if arr.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{arr[0]}."
+    arr.shift
+  end
+end
